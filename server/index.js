4be96d4891ea22b5a06be7e68ed6188dbeac3965
RfliFhri <raflifachriansyah43504@gmail.com>
2026-03-09 23:16:39 +0700

    revactor: error 404 route github v4

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ import convertRoutes from "./src/routes/convertRoutes.js";
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "https://rflifhri.github.io", 
+    "http://localhost:5173" // Agar tetap bisa ngetes di local
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
